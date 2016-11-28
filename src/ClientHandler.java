6266787669559e1f44397456b52925254e449dda
Sergey Padzhev <serhio_j@mail.ru>
2016-11-28 16:47:00 +0300

    Класс MyServer создан

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,5 +1,3 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Sergey Padzhev on 28.11.2016.
  */
--- src/MyServer.java
+++ src/MyServer.java
@@ -1,7 +1,38 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * Created by Sergey Padzhev on 28.11.2016.
  */
 public class MyServer {
+    private ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>(); // список клиентов храним в ArrayList
+
+    public MyServer{
+        ServerSocket server = null;
+        Socket s = null;
+        try {
+            server = new ServerSocket(8189); //Создание сервера на порту 8189
+            System.out.println("Server started. Waiting for clients connection.");
+            while (true){
+                s = server.accept();
+                System.out.println("Client connected.");
+    // когда создаётся обработчик ему передаётся ссылка на сервер, на котором он работает
+                ClientHandler h = new ClientHandler(s, this);
+                clients.add(h); // добавляем обработчик в список клиентов
+                new Thread(h).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{   // Закрытие сервера
+            try {
+                server.close();
+                System.out.println("Server closed.");
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
